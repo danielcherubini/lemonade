8ad00e361eca765edcd746327030e473eeeaa602
Daniel Cherubini <daniel@cherubini.casa>
2022-04-21 13:22:47 +0200

    adding windows run stuff

---
+++ windows/install.ps1
@@ -0,0 +1,1 @@
+Register-ScheduledTask -TaskName "TASKNAME" -Trigger (New-ScheduledTaskTrigger -AtLogon) -Action (New-ScheduledTaskAction -Execute "${Env:WinDir}\System32\WindowsPowerShell\v1.0\powershell.exe" -Argument "-WindowStyle Hidden -Command `"& 'C:\lemonade\run.ps1'`"") -RunLevel Highest -Force;
---
+++ windows/run.ps1
@@ -0,0 +1,1 @@
+Start-Process "C:\lemonade\lemonade.exe" server -WindowStyle Hidden
